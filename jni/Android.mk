44bd897e92160f3f4cdfdca015a7000123b44d0d
darkinfusion <daddyfire366@gmail.com>
2018-11-15 14:30:21 -0700

    add

---
+++ jni/Android.mk
@@ -0,0 +1,46 @@
+ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+android_hardware_fm.cpp \
+ConfFileParser.cpp \
+ConfigFmThs.cpp \
+FmIoctlsInterface.cpp \
+FmPerformanceParams.cpp
+
+ifeq ($(BOARD_HAS_QCA_FM_SOC), "cherokee")
+LOCAL_CFLAGS += -DFM_SOC_TYPE_CHEROKEE
+endif
+LOCAL_LDLIBS += -ldl
+LOCAL_SHARED_LIBRARIES := \
+        libandroid_runtime \
+        libnativehelper \
+        liblog \
+        libcutils
+
+LOCAL_MODULE := libqcomfm_jni
+LOCAL_MODULE_TAGS := optional
+
+ifeq ($(TARGET_FM_LEGACY_PATCHLOADER),true)
+    LOCAL_CFLAGS += -DFM_LEGACY_PATCHLOADER
+endif
+
+ifeq ($(TARGET_QCOM_NO_FM_FIRMWARE),true)
+    LOCAL_CFLAGS += -DQCOM_NO_FM_FIRMWARE
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.qti.fm.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+
+LOCAL_INIT_RC := init.qti.fm.rc
+
+include $(BUILD_PREBUILT)
+
+endif # is-vendor-board-platform
---
+++ libfm_jni/Android.mk
@@ -0,0 +1,40 @@
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    FmIoctlsInterface.cpp \
+    ConfigFmThs.cpp \
+    FmPerformanceParams.cpp \
+    ConfFileParser.cpp \
+    FmRadioController.cpp \
+    LibfmJni.cpp
+
+LOCAL_C_INCLUDES := $(JNI_H_INCLUDE) \
+    frameworks/base/core/jni/include \
+    frameworks/base/include/media
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libdl \
+    liblog \
+    libmedia \
+    libnativehelper \
+
+ifeq ($(TARGET_FM_LEGACY_PATCHLOADER),true)
+    LOCAL_CFLAGS += -DFM_LEGACY_PATCHLOADER
+endif
+
+ifeq ($(TARGET_QCOM_NO_FM_FIRMWARE),true)
+    LOCAL_CFLAGS += -DQCOM_NO_FM_FIRMWARE
+endif
+LOCAL_CFLAGS += -Wno-unused-parameter
+
+LOCAL_MODULE := libfmjni
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+
+endif # QCOM_HARDWARE
